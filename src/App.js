16d7615925cb827bb059744f2679215cd524c705
henrique <henrique.rodrigues.silva@hotmail.com>
2019-11-22 19:54:14 -0300

    meu primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <table>
+        <tr>
+          <th>Nome</th>  
+          <th>Telefone</th>
+          <th>E-mail</th>
+          <th>Ações</th>
+        </tr>
+        <tr>
+          <td>Henrique</td>  
+          <td>951783899</td>
+          <td>teste@teste.com</td>
+          <td>
+            <button>Editar</button>
+            <button>Excluir</button>
+          </td>
+        </tr>
+        <tr>
+          <td>Alexandre</td>  
+          <td>11111111111</td>
+          <td>teste@teste.com</td>
+          <td>
+            <button>Editar</button>
+            <button>Excluir</button>
+          </td>
+        </tr>
+      </table>
+      <form>
+        <label>Nome</label>
+        <input type="text" />
+        <label>Telefone</label>
+        <input type="text" />
+        <label>E-mail</label>
+        <input type="email" />
+        <button>Salvar</button>
+      </form>
     </div>
   );
 }
